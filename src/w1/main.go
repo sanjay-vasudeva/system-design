197301abd7bd7bcd4bb17b121062dfb91f75f356
Sanjay Vasudeva <sanjayvasudevasocial@gmail.com>
2025-04-13 15:16:13 +0530

    server sent events

--- src/w1/main.go
+++ src/w1/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"net/http"
+)
+
 func main() {
 	//basic_single()
 	//basic_multi(10)
@@ -7,5 +11,8 @@ func main() {
 
 	//cp := NewConnectionPool(5)
 	// benchmarkNonPool(150)
-	benchmarkPool(10000)
+	// benchmarkPool(10000)
+
+	http.HandleFunc("/events", eventHandler)
+	http.ListenAndServe(":8080", nil)
 }
---
+++ src/w1/sse_api.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func eventHandler(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers to allow all origins. You may want to restrict this to specific origins in a production environment.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Expose-Headers", "Content-Type")
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	for i := 0; i < 10; i++ {
+		fmt.Fprintf(w, "data: %d\n", i)
+		time.Sleep(1 * time.Second)
+		w.(http.Flusher).Flush() // Flush the response to the client
+	}
+	// closeNotifier := w.(http.CloseNotifier).CloseNotify()
+	closeNotifier := r.Context().Done()
+	<-closeNotifier
+	fmt.Println("Client disconnected")
+}
